62f7479efb8aa6d93c94c215144f513153c6bebf
Alberto Alejandro Ponce Perales <ponceperales09@gmail.com>
2025-10-09 02:07:04 -0500

    feat: added login and register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth-pages/presentation/components/login/login.component';
+import { RegisterComponent } from './auth-pages/presentation/components/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+];
