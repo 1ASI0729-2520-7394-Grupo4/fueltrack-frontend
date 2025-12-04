4eb18ab001d30dd2614a872f010f93a72f79d6fc
Manuel SA <u201817507@upc.edu.pe>
2025-12-04 07:04:35 -0500

    feat: added fake api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   platformApiBaseUrl: 'https://fueltrack-backend-production.up.railway.app/api',
+  fakeApiBaseUrl: 'https://localhost:3000',
   platformApiTerminalsEndpointPath: '/terminals',
   platformApiProvidersEndpointPath: '/providers',
   platformApiOrdersEndpointPath: '/orders',
